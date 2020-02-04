c562e5d3ccd4df32795d746ab2b7e936f6923f69
yafernandes <alexandrino.fernandes@gmail.com>
2020-02-04 12:57:18 -0500

    Updated servlet urls

--- src/main/java/com/dd/Main.java
+++ src/main/java/com/dd/Main.java
@@ -8,9 +8,9 @@ class Main {
         Server server = new Server(8080);
         ServletHandler handler = new ServletHandler();
         server.setHandler(handler);
-        handler.addServletWithMapping(Log4jServlet.class, "/log4j");
-        handler.addServletWithMapping(Log4j2Servlet.class, "/log4j2");
-        handler.addServletWithMapping(Slf4jServlet.class, "/slf4j");
+        handler.addServletWithMapping(Log4jServlet.class, "/instrumented-logs/log4j");
+        handler.addServletWithMapping(Log4j2Servlet.class, "/instrumented-logs/log4j2");
+        handler.addServletWithMapping(Slf4jServlet.class, "/instrumented-logs/slf4j");
         server.start();
         server.join();
     }
