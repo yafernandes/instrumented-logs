fa3305ad39f43baaa04b376b3ef4fb2d91cead65
yafernandes <alexandrino.fernandes@gmail.com>
2020-02-04 12:57:02 -0500

    Updated jar filename

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 cp ~/java-lib/dd-java-agent-0.42.0.jar dd-java-agent.jar
-cp ../build/libs/InstrLogs-all.jar application.jar
+cp ../build/libs/instrumented-logs-all.jar application.jar
 docker build . -t yaalexf/instrumented-logs
 docker push yaalexf/instrumented-logs
